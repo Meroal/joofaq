1172cda3cbea7ab1b81c65eab73daa255df41f28
joofsh <jonathanpagano@gmail.com>
2012-11-10 17:56:46 -0500

    Reorganize joofaq generator initialize

--- lib/generators/joofaq/joofaq_generator.rb
+++ lib/generators/joofaq/joofaq_generator.rb
@@ -1,16 +1,13 @@
 class JoofaqGenerator < Rails::Generators::Base
   source_root File.expand_path('../templates', __FILE__)
 
-
-  def generate_faq
+  def copy_faq_templates
     copy_file "joofaq.yml", "db/faq.yml"
     copy_file "joofaq/index.html.erb", "app/views/faq/index.html.erb"
     copy_file "joofaq/_qapair.html.erb", "app/views/faq/_qapair.html.erb"
     copy_file "joofaq/_subtitle.html.erb", "app/views/faq/_subtitle.html.erb"
-    create_route
   end
 
-private
   def create_route
     route "match '/faq' => 'Faq#index'"
   end
