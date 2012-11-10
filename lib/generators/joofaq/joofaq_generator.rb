61bb04d8647c67b38beafc5dd1ba19f9c9d18854
joofsh <jonathanpagano@gmail.com>
2012-11-10 16:32:43 -0500

    Add ridscount to Gemfile from joofaq generator

--- lib/generators/joofaq/joofaq_generator.rb
+++ lib/generators/joofaq/joofaq_generator.rb
@@ -8,9 +8,16 @@ class JoofaqGenerator < Rails::Generators::Base
     copy_file "joofaq/_qapair.html.erb", "app/views/faq/_qapair.html.erb"
     copy_file "joofaq/_subtitle.html.erb", "app/views/faq/_subtitle.html.erb"
     create_route
+    add_gem_dependencies
   end
+
+private
 
   def create_route
     route "match '/faq' => 'Faq#index'"
+  end
+
+  def add_gem_dependencies
+    gem 'rdiscount', version: '1.6.8'
   end
 end
